36421df97af3a38b42721e9af2a4adddbf71371b
Ryan-HK <ryan9037hk@gmail.com>
2022-07-13 09:00:48 +0900

    add 43th Algorithm

--- Algorithm_Inflearn/inflearn42_insertion_sort/Main.java
+++ Algorithm_Inflearn/inflearn42_insertion_sort/Main.java
@@ -8,24 +8,24 @@ public class Main {
 
 		
 		
-		// 버블정렬 (Bubble Sort)
-		// 서로 인접한 두 원소를 비교해서 
-		// 조건에 따라 자리를 바꿔주는 정렬이다.
+		// 삽입 정렬 (Insertion Sort)
+		// 두번째 원소부터 시작해서, 앞쪽(왼쪽)의 원소들의 비교하여, 삽입할 위치를 지정합니다.
+		// 그 후, 원소를 뒤로 옮기고 지정한 자리에 자료를 삽입합니다.
+		
 		
 		// 정렬하고자 하는 배열안에서, 교환하는 재자리 정렬이다.
 		// 안정 정렬(Stable Sort) : 중복된 값은, 입력 순서와 동일하게 정렬
-		for(int i=0; i<n-1; i++) {
+		for(int i=1; i<n; i++) {
 			
+			int tmp = arr[i];
+			int prev = i-1;
 			
-			for(int j=0; j<n-i-1; j++) {
-				
-				if(arr[j] > arr[j+1]) {
-					int tmp = arr[j+1];
-					arr[j+1] = arr[j];
-					arr[j] = tmp;
-				}
-				
+			while( prev >= 0 && arr[prev] > tmp) {
+				arr[prev+1] = arr[prev];
+				prev--;
 			} // for
+			
+			arr[prev+1] = tmp;
 			
 		} // for
 
---
+++ Algorithm_Inflearn/inflearn43_eam/Main.java
@@ -0,0 +1,49 @@
+package inflearn43_eam;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	public static List<Integer> solution(int s, int n, int[] arr) {
+		LinkedList<Integer> list = new LinkedList<>();
+		
+		
+		for(int i=0; i<arr.length; i++) {
+		
+			if(list.contains((Integer)arr[i])) {
+				list.remove((Integer)arr[i]);
+				list.add(0, arr[i]);
+			} else {
+				list.add(0, arr[i]);
+			}
+			
+			if(list.size() > s) {
+				list.remove(s);
+			} // if
+				
+		} // for
+	
+		return list;
+	} // solution	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int s = sc.nextInt();
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		for(int x : solution(s, n, arr)) {
+			System.out.print(x + " ");
+		}
+		
+	} // main
+
+} // end class
